70e10533188b0d7a90e5ebddcd3375e23ffeec33
AYUSH9393 <123872432+AYUSH9393@users.noreply.github.com>
2024-01-01 15:53:44 +0530

    Add new file middleware and update code

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data=initData.data.map((obj) =>({...obj,owner:"658ec86e62ce2eb1d649b149"}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
